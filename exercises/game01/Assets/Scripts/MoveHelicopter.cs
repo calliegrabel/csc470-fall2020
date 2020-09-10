1832ebc0bbc6096ec57940cc5e7f80288f244dd0
calliegrabel <callie.grabel@hotmail.com>
2020-09-10 16:15:45 -0400

    made a new game01

---
+++ exercises/game01/Assets/Scripts/MoveHelicopter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveHelicopter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = transform.position.x + transform.forward.x;
+        float y = transform.position.y + transform.forward.y;
+        float z = transform.position.z + transform.forward.z;
+        transform.position = new Vector3(x, y, z);
+        
+    }
+}
