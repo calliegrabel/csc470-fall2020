58fe7c2a1d23709f07cb2eff39616194a3322968
calliegrabel <callie.grabel@hotmail.com>
2020-10-29 16:17:15 -0400

    game 05

---
+++ exercises/game05/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject treePrefab;
+    public GameObject chickenPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            float x = Random.Range(-200, 200);
+            float z = Random.Range(-200, 200);
+            Vector3 pos = new Vector3(x, 0, z);
+            GameObject tree = Instantiate(treePrefab, pos, Quaternion.identity);
+            tree.transform.Rotate(0, Random.Range(0, 360), 0);
+        }
+        for (int i = 0; i < 100; i++)
+        {
+            float x = Random.Range(-200, 200);
+            float z = Random.Range(-200, 200);
+            Vector3 pos = new Vector3(x, 0, z);
+            GameObject chicken = Instantiate(chickenPrefab, pos, Quaternion.identity);
+            chicken.transform.Rotate(0, Random.Range(0, 360), 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ exercises/game05/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float moveSpeed = 10;
+    float rotateSpeed = 85;
+    public CharacterController cc;
+
+    bool prevIsGrounded = false;
+
+    float yVelocity = 0;
+    float jumpForce = 0.2f;
+    float gravityModifier = 0.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = gameObject.GetComponent<CharacterController>();
+    }   
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, hAxis * rotateSpeed * Time.deltaTime, 0, Space.Self);
+        if (!cc.isGrounded)
+        {
+            yVelocity = yVelocity + Physics.gravity.y * gravityModifier * Time.deltaTime;
+            if (Input.GetKeyUp(KeyCode.Space) && yVelocity > 0)
+            {
+                yVelocity = 0;
+            }
+        }
+
+        else
+        {
+            if (prevIsGrounded)
+            {
+                yVelocity = 0;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                yVelocity = jumpForce;
+            }
+        }
+        Vector3 amountToMove = transform.forward * vAxis * moveSpeed * Time.deltaTime;
+
+        amountToMove.y = yVelocity;
+
+
+        cc.Move(amountToMove);
+
+        prevIsGrounded = cc.isGrounded;
+    }
+
+}
